732dc4fbb0902a7cc03f58a82f7d6b19a1e67327
Administrator <1353154018@qq.com>
2020-09-02 16:41:23 +0800

    lan

--- src/main/java/com/hyit/hospital/controller/RegistlevelController.java
+++ src/main/java/com/hyit/hospital/controller/RegistlevelController.java
@@ -1,8 +1,12 @@
 package com.hyit.hospital.controller;
 
+import com.github.pagehelper.PageInfo;
+import com.hyit.hospital.model.Registlevel;
 import com.hyit.hospital.service.RegistlevelService;
 import com.hyit.hospital.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +24,33 @@ public class RegistlevelController {
     @RequestMapping("selAll")
     public ResultVO selAll(){
         ResultVO resultVO = new ResultVO("",200,registlevelService.selAll());
+        return resultVO;
+    }
+    @RequestMapping("selectAll/{currPage}")
+    public PageInfo<Registlevel> selectAll(@PathVariable Integer currPage, @RequestBody Registlevel registlevel){
+        return registlevelService.selectAll(currPage,registlevel);
+    }
+    @RequestMapping("registlevelAdd")
+    public ResultVO add(@RequestBody Registlevel registlevel){
+        registlevelService.registlevelAdd(registlevel);
+        ResultVO resultVO=new ResultVO("",200);
+        return resultVO;
+    }
+    @RequestMapping("delById/{id}")
+    public ResultVO deById(@PathVariable Integer id){
+        registlevelService.delById(id);
+        ResultVO resultVO=new ResultVO("",200);
+        return resultVO;
+    }
+    @RequestMapping("update")
+    public ResultVO update(@RequestBody Registlevel registlevel){
+        registlevelService.update(registlevel);
+        ResultVO resultVO=new ResultVO("",200);
+        return resultVO;
+    }
+    @RequestMapping("selById/{id}")
+    public ResultVO selById(@PathVariable Integer id){
+        ResultVO resultVO=new ResultVO("",200,registlevelService.selById(id));
         return resultVO;
     }
 
--- src/main/java/com/hyit/hospital/service/Impl/RegistlevelServiceImpl.java
+++ src/main/java/com/hyit/hospital/service/Impl/RegistlevelServiceImpl.java
@@ -1,5 +1,8 @@
 package com.hyit.hospital.service.Impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.hyit.hospital.common.Common;
 import com.hyit.hospital.mapper.RegistlevelMapper;
 import com.hyit.hospital.model.Registlevel;
 import com.hyit.hospital.service.RegistlevelService;
@@ -21,5 +24,34 @@ public class RegistlevelServiceImpl implements RegistlevelService {
     @Override
     public List<Registlevel> selAll() {
         return registlevelMapper.selectAll();
+    }
+    @Override
+    public PageInfo<Registlevel> selectAll(Integer currPage, Registlevel registlevel) {
+        if (currPage == null){
+            currPage = 1;
+        }
+        PageHelper.startPage(currPage, Common.PAGE_SIZE);
+        PageInfo pageInfo = new PageInfo(registlevelMapper.selectAll(registlevel));
+        return pageInfo;
+    }
+
+    @Override
+    public void registlevelAdd(Registlevel registlevel) {
+        registlevelMapper.insert(registlevel);
+    }
+
+    @Override
+    public void delById(Integer id) {
+        registlevelMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(Registlevel registlevel) {
+        registlevelMapper.updateByPrimaryKey(registlevel);
+    }
+
+    @Override
+    public Registlevel selById(Integer id) {
+        return registlevelMapper.selectByPrimaryKey(id);
     }
 }
